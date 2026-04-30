6e2da7ce7616b17ddd3e1f92093ceb5d1f775fb4
ran <owlran@outlook.com>
2026-05-01 07:03:05 +0800

    remove example

--- example/vite-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="vite/client" />
-declare const __DEV__: boolean
-declare module '*.css' { const css: string; export default css }
-declare module '*.css?inline' { const css: string; export default css }
