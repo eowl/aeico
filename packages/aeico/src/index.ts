0cb85a8195b15cb6c0c02b9a47b82eae9f8c2522
Ran <owlran@outlook.com>
2026-05-13 07:09:47 +0800

    chore: update aeico-view re-export

--- packages/aeico/src/index.ts
+++ packages/aeico/src/index.ts
@@ -33,5 +33,5 @@ export type {
 export { prop, PROP_METADATA_KEY } from 'aeico-element';
 
 // View — rendering
-export { Reconciler, html, render, getActiveBuilder, tags } from 'aeico-view';
-export type { BuilderProps, RenderResult } from 'aeico-view';
+export { Reconciler, html, render, getReconciler, tags } from 'aeico-view';
+export type { TagProps, RenderResult } from 'aeico-view';
