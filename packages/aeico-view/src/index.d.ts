cc101952ed89e28ab8934ed5a39a9a1fa4919c5f
ran <owlran@outlook.com>
2026-08-31 14:26:39 +0800

    chore: remove useless files

--- packages/aeico-view/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Reconciler } from './reconciler';
-export type { TagProps, HTMLTags, SVGOnlyTags } from './reconciler';
-export { html, render, getReconciler, tags } from './renderer';
-export type { RenderResult } from './renderer';
--- packages/aeico-view/src/utils.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Convert camelCase to kebab-case (for element tag name resolution).
- * @example camelToKebab('aeButton') // => 'ae-button'
- */
-export declare function camelToKebab(str: string): string;
